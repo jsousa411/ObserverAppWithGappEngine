153dd81009a89b2083c5ff1974ef7e9daf026210
JP <jsousa411@gmail.com>
2012-03-25 23:13:28 -0700

    removed GWT buttons (add delete...)

--- CloudTask1-AppEngine/src/com/cloudtask1/server/DataStore.java
+++ CloudTask1-AppEngine/src/com/cloudtask1/server/DataStore.java
@@ -15,99 +15,100 @@ import javax.servlet.ServletContext;
  
 public class DataStore { 
  
- 
-  /** 
-   * Remove this object from the data store. 
-   */ 
-  public void delete(Long Long1) { 
-    PersistenceManager pm = PMF.get().getPersistenceManager(); 
-    try { 
-      Task item = pm.getObjectById(Task.class, Long1); 
-      pm.deletePersistent(item); 
-    } finally { 
-      pm.close(); 
-    } 
-  } 
- 
-  /** 
-     * Find a {@link Task} by id. 
-     * 
-     * @param id the {@link Task} id 
-     * @return the associated {@link Task}, or null if not found 
-     */ 
-    @SuppressWarnings("unchecked") 
-    public Task find(Long id) { 
-      if (id == null) { 
-        return null; 
-      } 
- 
-      PersistenceManager pm = PMF.get().getPersistenceManager(); 
-      try { 
-        Query query = pm.newQuery("select from " + Task.class.getName() 
-            + " where id==" + id.toString() + " && emailAddress=='" + getUserEmail() + "'"); 
-        List list = (List) query.execute(); 
-        if (list.size() == 0)
-			return null;
-		else
-			return (Task) list.get(0); 
-      } catch (RuntimeException e) { 
-        System.out.println(e); 
-        throw e; 
-      } finally { 
-        pm.close(); 
-      } 
-    } 
- 
-@SuppressWarnings("unchecked") 
-public List findAll() { 
-  PersistenceManager pm = PMF.get().getPersistenceManager(); 
-  try { 
-      Query query = pm.newQuery("select from " + Task.class.getName() 
-          + " where emailAddress=='" + getUserEmail() + "'"); 
-      List list = (List) query.execute(); 
-      if (list.size() == 0) { 
-          list.size(); 
-        } 
- 
-    return list; 
-  } catch (RuntimeException e) { 
-    System.out.println(e); 
-    throw e; 
-  } finally { 
-    pm.close(); 
-  } 
-} 
- 
-  /** 
-   * Persist this object in the datastore. 
-   */ 
-  public Task update(Task item) { 
-    //set the user id (not sure this is where we should be doing this) 
-    item.setUserId(getUserId()); 
-    item.setEmailAddress(getUserEmail()); 
-    if (item.getDueDate() == null) { 
-        Calendar c = Calendar.getInstance(); 
-        c.set(2011, 5, 11); 
-        item.setDueDate(c.getTime()); 
-    } 
-    PersistenceManager pm = PMF.get().getPersistenceManager(); 
-    try { 
-      pm.makePersistent(item); 
-      return item; 
-    } finally { 
-      pm.close(); 
-    } 
-  } 
- 
-  public static String getUserId() { 
-    UserService userService = UserServiceFactory.getUserService(); 
-    User user = userService.getCurrentUser(); 
-    return user.getUserId(); 
-  } 
- 
-  public static String getUserEmail() { 
-            UserService userService = UserServiceFactory.getUserService(); 
-            User user = userService.getCurrentUser(); 
-            return user.getEmail(); 
-          } 
+	  /**
+	   * Remove this object from the data store.
+	   */
+	  public void delete(Long id) {
+	    PersistenceManager pm = PMF.get().getPersistenceManager();
+	    try {
+	      Task item = pm.getObjectById(Task.class, id);
+	      pm.deletePersistent(item);
+	    } finally {
+	      pm.close();
+	    }
+	  }
+
+	  /**
+	     * Find a {@link Task} by id.
+	     *
+	     * @param id the {@link Task} id
+	     * @return the associated {@link Task}, or null if not found
+	     */
+	    @SuppressWarnings("unchecked")
+	    public Task find(Long id) {
+	      if (id == null) {
+	        return null;
+	      }
+
+	      PersistenceManager pm = PMF.get().getPersistenceManager();
+	      try {
+	        Query query = pm.newQuery("select from " + Task.class.getName()
+	            + " where id==" + id.toString() + " && emailAddress=='" + getUserEmail() + "'");
+	        List list = (List) query.execute();
+	        if (list.size() == 0)
+				return null;
+			else
+				return (Task) list.get(0);
+	      } catch (RuntimeException e) {
+	        System.out.println(e);
+	        throw e;
+	      } finally {
+	        pm.close();
+	      }
+	    }
+
+	@SuppressWarnings("unchecked")
+	public List findAll() {
+	  PersistenceManager pm = PMF.get().getPersistenceManager();
+	  try {
+	      Query query = pm.newQuery("select from " + Task.class.getName()
+	          + " where emailAddress=='" + getUserEmail() + "'");
+	      List list = (List) query.execute();
+	      if (list.size() == 0) {
+	          list.size();
+	        }
+
+	    return list;
+	  } catch (RuntimeException e) {
+	    System.out.println(e);
+	    throw e;
+	  } finally {
+	    pm.close();
+	  }
+	}
+
+	  /**
+	   * Persist this object in the datastore.
+	   */
+	  public Task update(Task item) {
+	    //set the user id (not sure this is where we should be doing this)
+	    item.setUserId(getUserId());
+	    item.setEmailAddress(getUserEmail());
+	    if (item.getDueDate() == null) {
+	        Calendar c = Calendar.getInstance();
+	        c.set(2011, 5, 11);
+	        item.setDueDate(c.getTime());
+	    }
+	    PersistenceManager pm = PMF.get().getPersistenceManager();
+	    try {
+	      pm.makePersistent(item);
+	      return item;
+	    } finally {
+	      pm.close();
+	    }
+	  }
+
+	  public static String getUserId() {
+	    UserService userService = UserServiceFactory.getUserService();
+	    User user = userService.getCurrentUser();
+	    return user.getUserId();
+	  }
+
+	  public static String getUserEmail() {
+	            UserService userService = UserServiceFactory.getUserService();
+	            User user = userService.getCurrentUser();
+	            return user.getEmail();
+	          }
+
+
 }
